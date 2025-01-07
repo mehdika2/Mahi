63c4771dc54fe5d0939a369cd4bfa3cd9fd5f1a4
mahdi khalilzadeh <mahdiwich86@gmail.com>
2025-01-07 09:44:53 +0330

    Dynamic routing

--- Core/RouteUri.cs
+++ Core/RouteUri.cs
@@ -41,7 +41,18 @@ namespace Mahi.Core
 
 					case "dynamic":
 
-						break;
+						if (route.Value.RoutePath != null)
+							filename = ProcessDynamicRoute(route.Value.Url, uri.AbsolutePath, route.Value.RoutePath);
+						else if (route.Value.Redirect != null)
+						{
+							filename = ProcessDynamicRoute(route.Value.Url, uri.AbsolutePath, route.Value.Redirect);
+							if (filename == null)
+								break;
+							filename = filename.Insert(0, "~");
+						}
+						else throw new InvalidDataException("No route or redirect path set for static route: " + route.Key);
+
+						return true;
 
 					default:
 						throw new InvalidDataException("Invalid route type: " + route.Value.Type);
@@ -50,6 +61,35 @@ namespace Mahi.Core
 
 			filename = Path.GetFullPath("wwwapp") + '\\' + uri.AbsolutePath.Trim('/').Replace('/', '\\');
 			return false;
+		}
+
+		private static string ProcessDynamicRoute(string pattern, string url, string outputTemplate)
+		{
+			string regexPattern = Regex.Replace(pattern, @"\{([^}]+)\}", @"([^/]+)");
+
+			Regex regex = new Regex($"^{regexPattern}$");
+
+			Match match = regex.Match(url);
+
+			if (match.Success)
+			{
+				var parameters = new Dictionary<string, string>();
+
+				var paramNames = Regex.Matches(pattern, @"\{([^}]+)\}");
+				for (int i = 0; i < paramNames.Count; i++)
+				{
+					string paramName = paramNames[i].Groups[1].Value;
+					string paramValue = match.Groups[i + 1].Value;
+					parameters[paramName] = paramValue;
+				}
+
+				string result = outputTemplate;
+				foreach (var param in parameters)
+					result = result.Replace($"{{{param.Key}}}", param.Value);
+
+				return result;
+			}
+			else return null;
 		}
 	}
 }
